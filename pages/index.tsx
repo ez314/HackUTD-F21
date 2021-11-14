480cda5627acf219e819f823704e2f330d486fe7
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-11-13 21:35:11 -0600

    Add Firestore
    Make sure to add firebase.json to the root directory.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,28 @@ import UserContainer from '../components/UserContainer';
 import MainContainer from '../components/MainContainer';
 import GlobalContainer from '../components/GlobalContainer';
 
-export default function Home() {
+export async function getServerSideProps() {
+  const {db} = require('../util/Firebase');
+  const stocksRef = db.collection('stocks');
+  const snapshot = await stocksRef.get();
+
+  let stocks = {}
+  snapshot.forEach(doc => {
+    stocks[doc.id] = doc.data();
+  });
+
+  return {
+    props: {stocks}
+  }
+}
+
+export default function Home({stocks}) {
+  console.debug(stocks);
   return (
     <AppFrame>
-      <UserContainer user="test" subscribedStocks={[]} />
-      <MainContainer data={undefined} />
-      <GlobalContainer />
+      <UserContainer user="test" subscribedStocks={[]}/>
+      <MainContainer data={undefined}/>
+      <GlobalContainer/>
     </AppFrame>
   )
 }
