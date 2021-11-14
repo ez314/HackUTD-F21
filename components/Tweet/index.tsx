2d1c4dd47062a9279ac19ba529c6e2409a79963f
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2021-11-13 18:51:08 -0600

    feat(tweet): Added tweet component

---
+++ components/Tweet/index.tsx
@@ -0,0 +1,36 @@
+import LikeIcon from "../icon/LikeIcon";
+import RetweetIcon from "../icon/RetweetIcon";
+
+interface TweetProps {
+  photo: string;
+  name: string;
+  handle: string;
+  content: string;
+  likes: number;
+  retweets: number;
+  date: string;
+}
+export default function Tweet({ photo, name, handle, content, likes, retweets, date }: TweetProps) {
+  return (
+    <div className="flex flex-col w-64 m-auto rounded-xl bg-indigo-500 text-white p-2">
+      <div className="flex flex-row items-center justify-start w-full">
+        <img src={photo} className="w-10 h-10 rounded-full mx-3" />
+        <div className="flex flex-col text-sm">
+          <div className="m-0">{name}</div>
+          <div className="text-xs">{handle}</div>
+        </div>
+      </div>
+      <div className="text-xs w-full m-3">{content}</div>
+      <div className="flex flex-row items-center w-full">
+        <div className="flex flex-row items-center w-min mx-3">
+          <LikeIcon className="w-4 h-4" />
+          <div className="text-xs ml-1">{likes}</div>
+        </div>
+        <div className="flex flex-row items-center w-min mx-3">
+          <RetweetIcon className="w-4 h-4" />
+          <div className="text-xs ml-1">{retweets}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
